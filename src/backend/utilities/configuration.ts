7abfb1b286a49d7835eb9f2a50927cf3da11daa8
lukeg90 <lukegrunau@gmail.com>
2023-04-20 16:35:40 +0200

    Configure deployments

--- src/backend/utilities/configuration.ts
+++ src/backend/utilities/configuration.ts
@@ -6,9 +6,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { env } = process;
 
+const httpAuthPassword = env.SECRET_HTTP_AUTH_PASSWORD;
+
 export const configuration = {
   port: env.PORT || 3000,
   host: env.HOST || '127.0.0.1',
   isProduction: env.NODE_ENV === 'production',
   distFolder: path.join(cwd(), 'dist', 'frontend'),
+  httpAuthPassword,
 };
