5c6e5b9536e4358ef57bf0f06e9efa6696c32a07
Tom Adler <me@arty.name>
2023-05-23 12:07:58 +0200

    debug

--- testing/globalSetup.ts
+++ testing/globalSetup.ts
@@ -37,7 +37,7 @@ export default async function globalSetup() {
   }
 
   // configure the tests to talk to a new Astro instance
-  globalShared.server = spawn('yarn', ['dev'], {
+  globalShared.server = spawn('which', ['yarn'], {
     detached: true,
     env: { MODE: 'test', DATABASE_URI: process.env.DATABASE_URI },
   });
@@ -47,7 +47,7 @@ export default async function globalSetup() {
     });*/
     globalShared.server.stdout?.on('data', async (buffer: Buffer) => {
       const text = buffer.toString('utf-8');
-      // console.log(text);
+      console.log(text);
       const match = text.match(/http:\S+/);
       if (!match) {
         return;
