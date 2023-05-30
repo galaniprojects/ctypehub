ecde2803fd60c70ee47c6ce05fc1cd073fa02fce
lukeg90 <lukegrunau@gmail.com>
2023-05-30 17:48:14 +0200

    Make technical details declarative

--- testing/getSnapshotHtmlForPath.ts
+++ testing/getSnapshotHtmlForPath.ts
@@ -3,6 +3,9 @@ import { format } from 'prettier';
 export async function getSnapshotHtmlForPath(path: string) {
   const response = await fetch(`${process.env.URL}test/${path}`);
   const html = await response.text();
-  const pure = html.replace(/^.*<\/script>/s, '').replace(/\bastro-\w+/g, '');
+  const pure = html
+    .replace(/^.*<\/script>/s, '')
+    .replace(/\bastro-\w+/g, '')
+    .replace(/ class=""/g, '');
   return format(pure, { parser: 'html' });
 }
