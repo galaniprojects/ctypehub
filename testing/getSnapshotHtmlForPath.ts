f575b6604495ae316742274aae7672e0121d575e
Tom Adler <me@arty.name>
2023-06-06 13:23:46 +0200

    stable class names in snapshots (#89)

--- testing/getSnapshotHtmlForPath.ts
+++ testing/getSnapshotHtmlForPath.ts
@@ -6,6 +6,7 @@ export async function getSnapshotHtmlForPath(path: string) {
   const pure = html
     .replace(/^.*<\/script>/s, '')
     .replace(/\bastro-\w+/g, '')
+    .replace(/_[a-z0-9]{5}_\d/g, '')
     .replace(/ class=""/g, '');
   return format(pure, { parser: 'html' });
 }
