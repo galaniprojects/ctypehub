70fbcee7ae62d4a8e1f977bb0e85054a9d2f8c5a
Tom Adler <me@arty.name>
2023-06-06 17:13:20 +0200

    test index page

---
+++ src/components/index/index.test.ts
@@ -0,0 +1,17 @@
+import { beforeEach, describe, expect, it } from '@jest/globals';
+
+import { getSnapshotHtmlForPath } from '../../../testing/getSnapshotHtmlForPath';
+import { CType as CTypeModel } from '../../models/ctype';
+import { mockCTypes } from '../../utilities/mockCTypes';
+
+beforeEach(async () => {
+  await CTypeModel.destroy({ truncate: true });
+  await CTypeModel.bulkCreate([mockCTypes.example, mockCTypes.nestedProperty]);
+});
+
+describe('index.astro', () => {
+  it('should render all CTypes', async () => {
+    const html = await getSnapshotHtmlForPath('');
+    expect(html).toMatchSnapshot();
+  });
+});
