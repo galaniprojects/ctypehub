d41d3f24364984da99f3f0166710e1b137521f1d
Tom Adler <me@arty.name>
2023-06-23 14:55:35 +0200

    create CType: window.kilt type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,19 @@
 /// <reference types="astro/client" />
 
+import type { DidResourceUri, KiltAddress } from '@kiltprotocol/sdk-js';
+
 declare module 'jest-serializer-vue';
+
+declare global {
+  interface Window {
+    kilt: Record<
+      string,
+      {
+        signExtrinsicWithDid: (
+          extrinsic: string,
+          signer: KiltAddress,
+        ) => Promise<{ signed: string; didKeyUri: DidResourceUri }>;
+      }
+    >;
+  }
+}
