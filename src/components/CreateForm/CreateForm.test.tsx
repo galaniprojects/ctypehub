c271fa5ce73174dbd17eeab7638700ced11b1868
Tom Adler <me@arty.name>
2023-07-03 10:59:45 +0200

    create CType: fix a test

--- src/components/CreateForm/CreateForm.test.tsx
+++ src/components/CreateForm/CreateForm.test.tsx
@@ -8,11 +8,11 @@ import { CreateForm } from './CreateForm';
 
 describe('CreateForm', () => {
   it('should render', async () => {
-    const { container, getByLabelText, getByText } = render(<CreateForm />);
+    const { container, queryAllByLabelText, getByText } = render(<CreateForm />);
     expect(container).toMatchSnapshot();
 
     await userEvent.click(getByText(/Add Property/));
-    await waitFor(() => getByLabelText('Type:'));
+    await waitFor(() => queryAllByLabelText('Type:').length === 1);
     expect(container).toMatchSnapshot();
   });
 });
