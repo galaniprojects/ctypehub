a71fc8df9f36444bf29958a455a16c482d8a2e3d
Tom Adler <me@arty.name>
2023-07-13 11:26:13 +0200

    foreign keys: migration

---
+++ src/migrations/20230712145719-foreignKeys.cjs
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.addConstraint('Attestations', {
+      type: 'foreign key',
+      name: 'Attestations_cTypeId_fkey',
+      fields: ['cTypeId'],
+      references: { table: 'CTypes', field: 'id' },
+    });
+    await queryInterface.addConstraint('Tags', {
+      type: 'foreign key',
+      name: 'Tags_cTypeId_fkey',
+      fields: ['cTypeId'],
+      references: { table: 'CTypes', field: 'id' },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeConstraint('Attestations', 'Attestations_cTypeId_fkey');
+    await queryInterface.removeConstraint('Attestations', 'Tags_cTypeId_fkey');
+  },
+};
