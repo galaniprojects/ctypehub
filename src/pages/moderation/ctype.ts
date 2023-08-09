dbc73f398fc4ab005a68cb8954d329105c858bd0
Tom Adler <me@arty.name>
2023-08-09 22:20:25 +0200

    moderation pagination implementation (#230)

---
+++ src/pages/moderation/ctype.ts
@@ -0,0 +1,39 @@
+import type { APIContext } from 'astro';
+
+import { StatusCodes } from 'http-status-codes';
+import { Op } from 'sequelize';
+
+import { getSession } from '../session';
+import {
+  handleErrorResponse,
+  makeErrorResponse,
+} from '../../utilities/errorResponses';
+import { CType } from '../../models/ctype';
+
+export async function get({ request, url }: APIContext) {
+  try {
+    const session = getSession(request);
+    if (!session.authorized) {
+      throw makeErrorResponse('Unauthorized', StatusCodes.UNAUTHORIZED);
+    }
+
+    const before = url.searchParams.get('before');
+
+    const cTypes = (
+      await CType.findAll({
+        where: {
+          createdAt: { [Op.lte]: before ? new Date(before) : new Date() },
+        },
+        order: [['createdAt', 'DESC']],
+        include: 'tags',
+        limit: 20,
+      })
+    ).map(({ dataValues }) => dataValues);
+
+    return new Response(JSON.stringify(cTypes), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (exception) {
+    return handleErrorResponse(exception);
+  }
+}
