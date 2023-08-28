7b5f56dd69a26da28eafb36faf1b1ae70b47f31c
lukeg90 <lukegrunau@gmail.com>
2023-08-28 13:51:50 +0200

    Pagination bugfix

--- src/utilities/paginate.ts
+++ src/utilities/paginate.ts
@@ -13,7 +13,7 @@ export async function paginate(
 
   const isHomePage = !page || !Number.isInteger(Number(page));
 
-  const total = await CType.count({ where, include });
+  const total = await CType.count({ where });
   const lastPage = Math.floor(total / pageLimit);
 
   const currentPage = isHomePage ? lastPage : Number(page);
