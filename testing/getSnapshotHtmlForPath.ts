380e70035ec2437732257b7c59e1670e26db33ce
Tom Adler <me@arty.name>
2023-08-31 14:24:43 +0200

    updated snapshots

--- testing/getSnapshotHtmlForPath.ts
+++ testing/getSnapshotHtmlForPath.ts
@@ -5,9 +5,9 @@ export async function getSnapshotHtmlForPath(path: string) {
   const html = await response.text();
   const pure = html
     .replace(/^.*<\/script>/s, '')
-    .replace(/\bastro-\w+/g, '')
-    .replace(/_[a-z0-9]{5}_\d/g, '')
-    .replace(/ class=""/g, '')
+    .replace(/\bdata-astro-cid-\w+/g, '')
+    .replace(/\/@fs.*(\/src\/.*)\?.*?"/g, '$1"')
+    .replace(/_[a-z0-9]{5}_\d+/g, '')
     .replace('</head>', '')
     .replace('</html>', '');
   return format(pure, { parser: 'html' });
