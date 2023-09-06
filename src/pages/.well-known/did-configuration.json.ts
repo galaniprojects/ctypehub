d8ce1f989d715e56a59a2159a40c28c05f2c91a3
lukeg90 <lukegrunau@gmail.com>
2023-09-06 17:59:46 +0200

    Add well-known DID config for staging and prod

--- src/pages/.well-known/did-configuration.json.ts
+++ src/pages/.well-known/did-configuration.json.ts
@@ -69,6 +69,72 @@ const credentials: Record<string, unknown> = {
       },
     ],
   },
+  'http://stg.ctypehub.galaniprojects.de': {
+    '@context': 'https://identity.foundation/.well-known/did-configuration/v1',
+    linked_dids: [
+      {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          'https://identity.foundation/.well-known/did-configuration/v1',
+        ],
+        issuer: 'did:kilt:4sDiLJeaqE5H3RTcQGFJSKeNMDcQdUVhg5unDsLC6CREfPTp',
+        issuanceDate: '2023-09-06T15:52:44.313Z',
+        expirationDate: '2028-09-04T15:52:44.313Z',
+        type: [
+          'VerifiableCredential',
+          'DomainLinkageCredential',
+          'KiltCredential2020',
+        ],
+        credentialSubject: {
+          id: 'did:kilt:4sDiLJeaqE5H3RTcQGFJSKeNMDcQdUVhg5unDsLC6CREfPTp',
+          origin: 'https://stg.ctypehub.galaniprojects.de',
+          rootHash:
+            '0xd7b841847a672709e1d4cdfcae3a3b9e0d1ad3a6c657e20e77c455764f03e9c3',
+        },
+        proof: {
+          type: 'KILTSelfSigned2020',
+          proofPurpose: 'assertionMethod',
+          verificationMethod:
+            'did:kilt:4sDiLJeaqE5H3RTcQGFJSKeNMDcQdUVhg5unDsLC6CREfPTp#0x8f89e1f8d9ffb44327a4c9c8d62b0e7b42a41a3d558c9f3f2b21d4469ec488a7',
+          signature:
+            '0x13a98d22dee14fab3ed1f3b0cf4ee75fbed7cae01b878fc8c538fcd4a52527ff3df8e3ece22c40b74d39c0dec8bafa02fcda53b812047c5e218be3e7634c0d0f',
+        },
+      },
+    ],
+  },
+  'http://ctypehub.galaniprojects.de': {
+    '@context': 'https://identity.foundation/.well-known/did-configuration/v1',
+    linked_dids: [
+      {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          'https://identity.foundation/.well-known/did-configuration/v1',
+        ],
+        issuer: 'did:kilt:4sDiLJeaqE5H3RTcQGFJSKeNMDcQdUVhg5unDsLC6CREfPTp',
+        issuanceDate: '2023-09-06T15:57:46.170Z',
+        expirationDate: '2028-09-04T15:57:46.170Z',
+        type: [
+          'VerifiableCredential',
+          'DomainLinkageCredential',
+          'KiltCredential2020',
+        ],
+        credentialSubject: {
+          id: 'did:kilt:4sDiLJeaqE5H3RTcQGFJSKeNMDcQdUVhg5unDsLC6CREfPTp',
+          origin: 'https://ctypehub.galaniprojects.de',
+          rootHash:
+            '0x4a385f8de2e46fa6bf28694d333552abba5154311026de435e7b07120aead105',
+        },
+        proof: {
+          type: 'KILTSelfSigned2020',
+          proofPurpose: 'assertionMethod',
+          verificationMethod:
+            'did:kilt:4sDiLJeaqE5H3RTcQGFJSKeNMDcQdUVhg5unDsLC6CREfPTp#0x8f89e1f8d9ffb44327a4c9c8d62b0e7b42a41a3d558c9f3f2b21d4469ec488a7',
+          signature:
+            '0x2824ec105c866be5241cd7df92712e069b61b17525416c03fa010d48df5e79d0046021f7f6b63c0de4a9cbe5b955a69aa068b940128d7e6d2a380226851d1701',
+        },
+      },
+    ],
+  },
 };
 
 export async function GET({ url }: APIContext) {
