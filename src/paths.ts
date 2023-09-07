b6ddba621da0b01f741b3b10aa50ee34acb8b1df
lukeg90 <lukegrunau@gmail.com>
2023-09-07 10:42:02 +0200

    Legal docs

--- src/paths.ts
+++ src/paths.ts
@@ -8,6 +8,9 @@ export const paths = {
   moderationVerify: '/moderation/verify',
   moderationCTypes: '/moderation/ctype',
   moderationCType: '/moderation/ctype/[param]',
+  imprint: '/imprint',
+  terms: '/terms',
+  privacy: '/privacy',
 };
 
 export function generatePath(path: string, param: string) {
