d4db191ecdcce6a53eadb343bc8fddd75918e985
lukeg90 <lukegrunau@gmail.com>
2023-09-08 15:28:26 +0200

    do not initialize for prerendered routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,22 @@
+import type { APIContext } from 'astro';
+
 import { memoize } from 'lodash-es';
 
 import { initialize } from './utilities/initialize';
+import { paths } from './paths';
 
 const initializedOnce = memoize(initialize);
 
-export async function onRequest(unused: unknown, next: () => unknown) {
-  await initializedOnce();
+export async function onRequest(context: APIContext, next: () => unknown) {
+  const staticRoutes = [
+    `${paths.imprint}/`,
+    `${paths.terms}/`,
+    `${paths.privacy}/`,
+  ];
+
+  if (!staticRoutes.includes(context.url.pathname)) {
+    await initializedOnce();
+  }
+
   return await next();
 }
