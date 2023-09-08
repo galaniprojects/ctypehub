d23588b817c4fd147ee24a1be8463a90c4fb22bd
Luke Grunau <lukegrunau@gmail.com>
2023-09-08 16:03:00 +0200

    Revert "use astro:assets Image (#291)" (#310)
    This reverts commit 339d72df9b6f72d5c690669d887943974b3cc7f3.

--- testing/getSnapshotHtmlForPath.ts
+++ testing/getSnapshotHtmlForPath.ts
@@ -1,23 +1,12 @@
 import { format } from 'prettier';
 
-function getOriginalImagePath(input: string) {
-  return input.replace(/\/@fs.*(\/src\/.*)\?.*/g, '$1');
-}
-
 export async function getSnapshotHtmlForPath(path: string) {
   const response = await fetch(`${process.env.URL}${path}`);
   const html = await response.text();
   const pure = html
     .replace(/^.*<\/script>/s, '')
     .replace(/\bdata-astro-cid-\w+/g, '')
-    .replace(
-      /\/@fs.*(\/src\/.*)\?.*?"/g,
-      (match) => `${getOriginalImagePath(match)}"`,
-    )
-    .replace(/\/(_image\?href=.*?)"/g, (match) => {
-      const url = new URL(match, 'http://localhost');
-      return `${getOriginalImagePath(url.searchParams.get('href') as string)}"`;
-    })
+    .replace(/\/@fs.*(\/src\/.*)\?.*?"/g, '$1"')
     .replace(/_[a-z0-9]{5}_\d+/g, '')
     .replace('</head>', '')
     .replace('</html>', '');
