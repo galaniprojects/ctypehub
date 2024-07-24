3b0343916eefa0b1bfd2b2e93fcccaf4800cd0f8
kilted-andres <andres@kilt.io>
2024-07-24 21:48:34 +0200

    feat: good generic query

--- src/utilities/indexer/queryFromIndexer.ts
+++ src/utilities/indexer/queryFromIndexer.ts
@@ -1,13 +1,14 @@
 import { got } from 'got';
 
 import { configuration } from '../configuration';
+import { logger } from '../logger';
 
 const { indexer } = configuration;
 
 const queryBlocks = `
   query {
     blocks(orderBy: TIME_STAMP_DESC, first: 3) {
-      totalCount
+    totalCount
       nodes {
         id
         timeStamp
@@ -17,19 +18,58 @@ const queryBlocks = `
   }
 `;
 
+export interface FetchedData {
+  data: Record<
+    string,
+    {
+      totalCount?: number;
+      nodes: Array<Record<string, unknown>>;
+    }
+  >;
+}
+
 export async function queryFromIndexer(query: string = queryBlocks) {
   try {
-    const response = await got.post(indexer.graphqlEndpoint, {
-      json: {
-        query,
-      },
-      responseType: 'json',
-    });
+    // logger.info('indexer endpoint: ' + indexer.graphqlEndpoint);
 
-    console.log('Response from GraphQL', JSON.stringify(response.body));
+    const response = await got
+      .post(indexer.graphqlEndpoint, {
+        json: {
+          query,
+        },
+      })
+      .json<FetchedData>();
 
-    return response.body;
+    const entities = Object.entries(response.data);
+
+    entities.length > 1 &&
+      logger.error('Please, avoid multiple queries in a single request.');
+
+    const [name, { totalCount, nodes: matches }] = entities[0];
+
+    totalCount ??
+      logger.error(
+        'The query did not ask for total count. Please add field "totalCount" to your query.',
+      );
+
+    logger.info(
+      `Completed querying '${name}' from GraphQL under ${indexer.graphqlEndpoint}.`,
+    );
+
+    // logger.info('Response from GraphQL: ' + JSON.stringify(response, null, 2));
+
+    logger.info(
+      `Got ${matches.length} out of ${totalCount} '${name}' matching query.`,
+    );
+
+    // matches.forEach((match, index) => {
+    //   logger.info(
+    //     `#${index} match from query: ${JSON.stringify(match, null, 2)}`,
+    //   );
+    // });
+
+    return { totalCount, matches };
   } catch (error) {
-    console.error('Error:', error);
+    console.error(`Error Querying from ${indexer.graphqlEndpoint}:`, error);
   }
 }
