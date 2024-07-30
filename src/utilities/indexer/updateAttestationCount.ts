69cbf114e25319c2f6f320d74e21db43e755e7d3
kilted-andres <andres@kilt.io>
2024-07-30 18:53:31 +0200

    feat: function to update Attestation count

---
+++ src/utilities/indexer/updateAttestationCount.ts
@@ -0,0 +1,70 @@
+import { type ICType } from '@kiltprotocol/sdk-js';
+
+import { Op } from 'sequelize';
+
+import { CType as CTypeModel } from '../../models/ctype';
+
+import { logger } from '../logger';
+
+import { matchesGenerator } from './queryFromIndexer';
+
+const QUERY_SIZE = 50;
+
+export async function updateAttestationsCount() {
+  const savedCTypes = await CTypeModel.findAll({
+    order: [['createdAt', 'ASC']],
+    where: {
+      block: {
+        [Op.not]: null,
+      },
+    },
+  });
+
+  logger.info(`Updating Attestation Count for ${savedCTypes.length} CTypes.`);
+
+  // When modifying this query, first try it out on the https://indexer.kilt.io/ (or dev-indexer) and click on "Prettify"
+  const writtenQuery = `
+  query {
+    attestationsCount: cTypes(orderBy: ID_ASC, first: ${QUERY_SIZE}, offset: 0 ) {
+    # It does not matter what the order criterion is as long as it is consistent.
+      totalCount
+      nodes {
+        cTypeId: id
+        attestationsCreated
+        registrationBlockId
+      }
+    }
+  }
+  `;
+
+  interface QueriedAttestationCount {
+    cTypeId: ICType['$id'];
+    attestationsCreated: number;
+    registrationBlockId: string; // Block Ordinal Number, without punctuation
+  }
+  const entitiesGenerator = matchesGenerator(writtenQuery);
+
+  for await (const entity of entitiesGenerator) {
+    const { cTypeId, attestationsCreated } =
+      entity as unknown as QueriedAttestationCount;
+
+    const cTypeToUpdate = await CTypeModel.findOne({
+      where: {
+        id: {
+          [Op.eq]: cTypeId,
+          [Op.ne]: attestationsCreated,
+        },
+      },
+    });
+
+    if (!cTypeToUpdate) {
+      continue;
+    }
+
+    logger.info(
+      `Updating Attestation Count of cType "${cTypeToUpdate.getDataValue('id')}" from ${cTypeToUpdate.getDataValue('attestationsCount')} to ${attestationsCreated}`,
+    );
+    cTypeToUpdate.set('attestationsCount', attestationsCreated.toString(10));
+    await cTypeToUpdate.save();
+  }
+}
