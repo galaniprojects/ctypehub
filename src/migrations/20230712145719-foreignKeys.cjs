54d70c43b5bbe3bf681e501625aca7dd0d94adc7
kilted-andres <andres@kilt.io>
2024-08-06 23:11:20 +0200

    fix: remove residuals of Attestation Model

--- src/migrations/20230712145719-foreignKeys.cjs
+++ src/migrations/20230712145719-foreignKeys.cjs
@@ -3,12 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.addConstraint('Attestations', {
-      type: 'foreign key',
-      name: 'Attestations_cTypeId_fkey',
-      fields: ['cTypeId'],
-      references: { table: 'CTypes', field: 'id' },
-    });
     await queryInterface.addConstraint('Tags', {
       type: 'foreign key',
       name: 'Tags_cTypeId_fkey',
@@ -18,7 +12,6 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    await queryInterface.removeConstraint('Attestations', 'Attestations_cTypeId_fkey');
-    await queryInterface.removeConstraint('Attestations', 'Tags_cTypeId_fkey');
+    await queryInterface.removeConstraint('Tags', 'Tags_cTypeId_fkey');
   },
 };
