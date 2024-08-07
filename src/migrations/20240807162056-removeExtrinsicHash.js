b4f79b7cfbce02bbd4daf03c695f2d645662f553
kilted-andres <andres@kilt.io>
2024-08-07 18:23:04 +0200

    fix: remove property extrinsicHash from cTypes table

---
+++ src/migrations/20240807162056-removeExtrinsicHash.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('CTypes', 'extrinsicHash');
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.addColumn('CTypes', 'extrinsicHash', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+  },
+};
